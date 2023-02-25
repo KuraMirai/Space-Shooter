d626eb69ce2f4e54b709c59d98d78cf7d5a36494
sbielievtsov <astro3453@gmail.com>
2023-02-25 03:05:00 +0200

    Joystick, Spaceships, ships movement and rotation

--- Assets/Scripts/Player/PlayerShipMovement.cs
+++ Assets/Scripts/Player/PlayerShipMovement.cs
@@ -1,21 +1,46 @@
 using Managers;
+using UI.JoyStick;
 using UnityEngine;
 
 namespace Player
 {
     public class PlayerShipMovement : MonoBehaviour
     {
+        [SerializeField] private Joystick movementJoyStick;
         [SerializeField] private Rigidbody rb;
+        [SerializeField] private Transform shipAssetTransform;
         [SerializeField] private float speed = 2f;
+        [SerializeField] private float turnSpeed = 5;
+        [SerializeField] private bool useJoystickControls;
 
         private float horizontal;
         private float vertical;
         private Vector3 _movementVector;
+        
+
+        private void Awake()
+        {
+#if UNITY_ANDROID
+            useJoystickControls = true;
+#endif
+            movementJoyStick.Horizontal += OnHorizontalMovement;
+            movementJoyStick.Vertical += OnVerticalMovement;
+        }
 
         private void FixedUpdate()
         {
-            horizontal = Input.GetAxisRaw("Horizontal");
-            vertical = Input.GetAxisRaw("Vertical");
+            HandleMovement();
+            HandleRotation();
+            CheckPlayerInScreenBounds();
+        }
+
+        private void HandleMovement()
+        {
+            if (!useJoystickControls)
+            {
+                horizontal = Input.GetAxisRaw("Horizontal");
+                vertical = Input.GetAxisRaw("Vertical");
+            }
 
             _movementVector = new Vector3(horizontal, vertical, 0);
         
@@ -23,8 +48,22 @@ namespace Player
                 rb.velocity = _movementVector.normalized * speed;
             else
                 rb.velocity = Vector3.zero;
-        
-            CheckPlayerInScreenBounds();
+        }
+
+        private void HandleRotation()
+        {
+            if (horizontal != 0)
+            {
+                shipAssetTransform.rotation *= Quaternion.Euler(new Vector3(0, 0, -horizontal) * turnSpeed);
+                Vector3 rot = shipAssetTransform.rotation.eulerAngles;
+                rot.y = rot.y > 180 ? rot.y - 360 : rot.y;
+                rot.y = Mathf.Clamp(rot.y, -45, 45);
+                shipAssetTransform.rotation = Quaternion.Euler(rot);
+            }
+            else
+            {
+                shipAssetTransform.rotation = Quaternion.Slerp(shipAssetTransform.rotation, Quaternion.Euler(-90, 0, 180), turnSpeed * Time.deltaTime);
+            }
         }
 
         private void CheckPlayerInScreenBounds()
@@ -35,5 +74,15 @@ namespace Player
             playerPos.y = Mathf.Clamp(playerPos.y, -screenBounds.y, screenBounds.y);
             transform.position = playerPos;
         }
+        
+        private void OnHorizontalMovement(float input)
+        {
+            horizontal = input;
+        }
+        
+        private void OnVerticalMovement(float input)
+        {
+            vertical = input;
+        } 
     }
 }
